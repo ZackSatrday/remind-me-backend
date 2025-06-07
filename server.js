85f2acab97000cdd5e6da34d0a4fec26706cc5b8
ZackSatrday <sushant.kr398@gmail.com>
2025-06-07 10:04:52 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,28 @@
+import express from 'express'
+import cors from "cors"
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(cors())
+
+// post remainder date, time, message, type(sms/email)
+app.post('/api/reminder', (req, res) => {
+    const {date, time, message, method} = req.body
+
+    if(!date || !time || !message || !method){
+        return res.status(400)
+    }
+    // here we can add it to a database
+    console.log("New Reminder")
+    console.log(`Date: ${date}\nTime:${time}\nMessage:${message}\nMethod:${method}`)
+
+    res.json({
+        status : "success"
+    })
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
