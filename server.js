459a93c870df5cee75fa6df66b8fa15f991e29d5
ZackSatrday <sushant.kr398@gmail.com>
2025-06-07 10:36:57 +0530

    small change

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 import express from 'express'
-import cors from "cors"
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(express.json())
-app.use(cors())
 
 // post remainder date, time, message, type(sms/email)
 app.post('/api/reminder', (req, res) => {
